6c2b88a766b66ba559bcb7ba83b30baa6dbb5727
Wilou <10931470+Benliam12@users.noreply.github.com>
2025-12-30 00:18:37 -0500

    test: Adding some test

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,24 @@
-module.exports = {
-  preset: 'ts-jest',
+export default {
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+  roots: ['<rootDir>/tests'],  // Changed from src to tests
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
-  collectCoverageFrom: ['src/**/*.ts', '!src/**/*.d.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/**/*.test.ts',
+    '!src/**/*.spec.ts'
+  ],
 };
---
+++ tests/Basics.test.ts
@@ -0,0 +1,5 @@
+describe('Basic Module', () => {
+    test('should run a simple test and succeed', async () => {
+        expect(1 + 1).toBe(2);
+    });
+});
